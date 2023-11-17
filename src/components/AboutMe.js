57ce8ad9aedca55418aeda2d3f9c363d3b4928ca
bashir-tech <baashiisse3@gmail.com>
2023-11-17 16:43:18 +0300

    modified

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -10,21 +10,22 @@ function AboutMe() {
             </span>
             <div className="main">
                 <div className="up">
-                    <button href="https://www.instagram.com/bash_isse/" className="card1">
+                    <a href="https://www.instagram.com/bash_isse/" className="card1">
 
-                        <ion-icon href="https://www.instagram.com/bash_isse/" name="logo-instagram"></ion-icon>
-                    </button>
-                    <button className="card2">
+                        <ion-icon name="logo-instagram"></ion-icon>
+                    </a>
+                    <a href="https://twitter.com/isse_egaal" className="card2">
                         <ion-icon name="logo-twitter"></ion-icon>
-                    </button>
+                    </a>
                 </div>
                 <div className="down">
-                    <button className="card3">
+                    <a href='https://github.com/bashir-tech' className="card3">
                         <ion-icon name="logo-github"></ion-icon>
-                    </button>
-                    <button className="card4">
+                    </a>
+                    <a href='https://www.linkedin.com/in/bashir-ali-isse-28a734176/' className="card4">
+
                         <ion-icon name="logo-linkedin"></ion-icon>
-                    </button>
+                    </a>
                 </div>
             </div>
 
